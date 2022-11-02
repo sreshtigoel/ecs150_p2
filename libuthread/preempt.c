a673ca89fe3973ac291f33b8b867e30821b30a83
Omar Alsabbagh <42855230+Omar-Alsabbagh17@users.noreply.github.com>
2022-11-01 21:20:32 -0700

    finished phase 4

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -14,24 +14,58 @@
  * 100Hz is 100 times per second
  */
 #define HZ 100
+struct sigaction old_action; // to save old action of SIGVTALRM
+struct itimerval old_itimerval;
+
+void alarm_handler(int signum) {
+	(void) signum; // non-harmful code. to suppress compiler warning
+	uthread_yield();
+}
 
 void preempt_disable(void)
 {
-	/* TODO Phase 4 */
+	sigset_t ss;
+	sigemptyset(&ss);
+	sigaddset(&ss, SIGVTALRM);
+	sigprocmask(SIG_BLOCK, &ss, NULL);
 }
 
 void preempt_enable(void)
 {
-	/* TODO Phase 4 */
+	sigset_t ss;
+	sigemptyset(&ss);
+	sigaddset(&ss, SIGVTALRM);
+	sigprocmask(SIG_UNBLOCK, &ss, NULL);
 }
 
 void preempt_start(bool preempt)
 {
-	/* TODO Phase 4 */
+	if (preempt)
+	{
+		struct sigaction sa;
+		sa.sa_handler = alarm_handler;
+		sigemptyset(&sa.sa_mask);
+		sa.sa_flags = 0;
+
+		// change action of signal, and save old action
+		sigaction(SIGVTALRM, &sa, &old_action); 
+		
+		// alarm config
+		struct itimerval new_itimerval;
+		new_itimerval.it_interval.tv_sec = 0;
+		new_itimerval.it_interval.tv_usec = 100 * HZ; // 100 * HZ == 10 msec
+		new_itimerval.it_value.tv_sec = 0;
+		new_itimerval.it_value.tv_usec = 100 * HZ;
+		setitimer(ITIMER_VIRTUAL, &new_itimerval, &old_itimerval);
+
+	}
 }
 
 void preempt_stop(void)
 {
-	/* TODO Phase 4 */
+	//restore configuration
+	sigaction(SIGVTALRM, &old_action, NULL);
+	setitimer(ITIMER_VIRTUAL, &old_itimerval, NULL);
+
 }
 
